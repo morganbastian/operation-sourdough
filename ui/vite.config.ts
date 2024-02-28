51b74d2d270c099d14d0a1d2bc9a3671f1f9af8d
Morgan Bastian <morganbastian07@gmail.com>
2024-02-28 17:21:29 -0500

    installed vitest and setup test

--- ui/vite.config.ts
+++ ui/vite.config.ts
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import react from '@vitejs/plugin-react-swc';
+import { defineConfig as defineVitestConfig } from 'vitest/config'; 
 
-// https://vitejs.dev/config/
-export default defineConfig({
+
+export default defineVitestConfig({
   plugins: [react()],
-})
+  test: {
+    
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './src/setupTests.ts', 
+  },
+});
